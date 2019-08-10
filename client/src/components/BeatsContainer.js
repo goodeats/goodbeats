af458443ecb5ab8d253bc2aacafee510d32a088a
Pat <patrick.h.needham@gmail.com>
2019-08-10 19:33:09 -0400

    added beats container to client

---
+++ client/src/components/BeatsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BeatsContainer extends Component {
+  render() {
+    return (
+      <div className="Beats-container">
+        Beats
+      </div>
+    )
+  }
+}
+
+export default BeatsContainer;
