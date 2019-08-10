e74012df41161241308b166838b74e1e089ed39d
Pat <patrick.h.needham@gmail.com>
2019-08-10 19:15:36 -0400

    added namespacing to get resources at /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :instruments
-  resources :beats
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :beats
+      resources :instruments
+    end
+  end
+
 end
